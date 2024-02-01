e1634d1280caa122016df83ff4a4810f67919486
Ryan Zielke <NeoAlchemy@gmail.com>
2024-01-31 18:43:09 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,4 @@
+let quetzalcoatl = sprites.create(assets.image`left-facing-queztalcoatl`, SpriteKind.Player)
+quetzalcoatl.setScale(0.5, ScaleAnchor.Middle)
+controller.moveSprite(quetzalcoatl)
+let mySprite = sprites.create(assets.image`tamales`, SpriteKind.Food)
